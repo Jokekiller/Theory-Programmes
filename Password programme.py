a290b22be9ee8432e493b59573265c0060ccf3d0
Harry <Harry@192.168.0.8>
2014-10-02 12:07:36 +0100

    password programme

---
+++ Password programme.py
@@ -0,0 +1,8 @@
+#Harry Robinson
+#02-10-2014
+#Password programme
+
+potentialPassword = int(input("Enter the length of your desired password: "))
+set 'potentialPassword':
+    randrange(97,122)
+    
