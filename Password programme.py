0567050d12c94da9603df3aab50440f570624c0a
Harry <Harry@192.168.0.8>
2014-10-07 11:54:54 +0100

    Altered ASCII program

--- Password programme.py
+++ Password programme.py
@@ -7,7 +7,7 @@ import random
 passwordLength = int(input("Enter the length of your desired password: "))
 password = ''
 for index in range (0, passwordLength):
-    password = password + chr(random.randrange(97,122))
+    password = password + chr(random.randrange(48,57 and 65,90 and 97,122))
     #print(password)
 print(password)
     
